f274db81d15d7fa2a4a0c64a123320bc5088e8e3
Eren <24630742+arasharn@users.noreply.github.com>
2018-10-17 11:33:35 -0400

    Create 5headerfile.cpp

---
+++ 5headerfile/5headerfile.cpp
@@ -0,0 +1,19 @@
+/*Goal: practice using header files.
+**. 
+**Put in the header file every thing that is related to 
+**"how to do a task".
+**Put the "what to do" in the main program.
+*/
+
+/*
+#include <iostream>
+#include <string>
+
+using namespace std;
+*/
+#include "main.hpp"
+int main()
+{
+    cout<<"Hello, I use header files!";
+    return 0;
+}
