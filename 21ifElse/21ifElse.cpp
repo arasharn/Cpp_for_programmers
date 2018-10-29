a30f01695a1611376766cdcf7050b773b878a2cb
Eren <24630742+arasharn@users.noreply.github.com>
2018-10-29 09:20:14 -0400

    Create 21ifElse.cpp

---
+++ 21ifElse/21ifElse.cpp
@@ -0,0 +1,29 @@
+
+#include<iostream>
+
+int main()
+{
+    int TARGET = 33;
+    int guess;
+    std::cout<<"Guess a number between 0 - 100\n";
+    std::cin>>guess;
+    
+    std::cout<<"You guessed: "<<guess<<"\n";
+    
+    
+    if(guess < TARGET)
+    {
+        std::cout<<"Your guess is too low.\n";
+    }
+    else if(guess > TARGET)
+    {
+        std::cout<<"Your guess is too high.\n";
+    }
+    else
+    {
+        std::cout<<"Yay! You guessed correctly.\n";
+    }
+    
+    
+    return 0;
+}
